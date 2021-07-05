b669a130a273a8a989fa42587f7a636fefc5b35b
Evan BOISSONNOT <evan@boissonnot.fr>
2021-07-05 16:58:51 +0200

    fix(gameservice): add mapping

--- starwars-game/projects/game/src/app/features/game/services/game.service.ts
+++ starwars-game/projects/game/src/app/features/game/services/game.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { api } from 'projects/game/src/environments/api';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { GameDto } from '../../../core/models/game.dto';
+
+export declare type ResultApi = {
+    state: string,
+    items: GameDto[]
+}
 
 @Injectable()
 export class GameService {
@@ -10,6 +16,8 @@ export class GameService {
   constructor(private httpClient: HttpClient) { }
 
   getAll(nbItems: number = 100): Observable<GameDto[]> {
-    return this.httpClient.get<GameDto[]>(api.games.url + '?max=' + nbItems);
+    return this.httpClient.get<ResultApi>(api.games.url + '?max=' + nbItems).pipe(
+      map(item => [...item.items])
+    );
   }
 }
