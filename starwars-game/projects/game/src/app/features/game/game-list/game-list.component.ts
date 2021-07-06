58c1fdebbff6357d3277802df18dc5caf5544a87
Evan BOISSONNOT <evan@boissonnot.fr>
2021-07-06 11:08:58 +0200

    feat(observable): presenting observable

--- starwars-game/projects/game/src/app/features/game/game-list/game-list.component.ts
+++ starwars-game/projects/game/src/app/features/game/game-list/game-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { GameDto } from '../../../core/models/game.dto';
 import { GameService } from '../services/game.service';
 
@@ -8,14 +8,59 @@ import { GameService } from '../services/game.service';
   templateUrl: './game-list.component.html',
   styleUrls: ['./game-list.component.css']
 })
-export class GameListComponent implements OnInit {
+export class GameListComponent implements OnInit, OnDestroy {
   games: GameDto[] = [];
   searchItem = '';
   itemsPerPage = [ 10, 25, 50, 100 ];
+  private subscriptions: Subscription[] = [];
+
   constructor(private gameService: GameService) { }
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(item => item.unsubscribe());
+  }
+
   ngOnInit(): void {
-    this.gameService.getAll(3).subscribe(items => this.games = items);
+    const obs$ = this.gameService.getAll(3);
+
+    this.subscriptions.push(obs$.subscribe(items => this.games = items));
+
+    this.subscriptions.push(obs$.subscribe(items => this.games = items));
+    this.subscriptions.push(obs$.subscribe(items => this.games = items ));
+
+    // Producteur
+    const monObservable$ = new Observable(observer => {
+      console.info('hey je m\'execute', Math.random());
+
+      observer.next(1);
+      observer.next(2);
+
+      setTimeout(() => {
+        observer.next(30);
+      }, 1500);
+
+      setTimeout(() => {
+        observer.next(40);
+      }, 0);
+
+      observer.next(3);
+      observer.complete();
+      observer.next(5);
+    });
+
+
+    // Consommateur
+    monObservable$.subscribe();
+    monObservable$.subscribe();
+
+    monObservable$.subscribe(
+    item => {
+      console.info('conso:', item);
+    },
+    error => {},
+    () => console.info('complete ici'));
+
+    console.info('ici ???');
   }
 
 }
--- starwars-game/projects/game/src/app/features/game/services/game.service.ts
+++ starwars-game/projects/game/src/app/features/game/services/game.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { api } from 'projects/game/src/environments/api';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { GameDto } from '../../../core/models/game.dto';
 
 export declare type ResultApi = {
@@ -16,7 +16,10 @@ export class GameService {
   constructor(private httpClient: HttpClient) { }
 
   getAll(nbItems: number = 100): Observable<GameDto[]> {
+    const random = Math.random();
+
     return this.httpClient.get<ResultApi>(api.games.url + '?max=' + nbItems).pipe(
+      tap(item => console.info(random)),
       map(item => [...item.items])
     );
   }
