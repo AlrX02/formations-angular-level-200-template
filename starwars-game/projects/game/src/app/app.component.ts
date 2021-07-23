170ba38ec0e653556172548f8e301e180ca41740
Oudot Alrick <alrick.oudot@inra.fr>
2021-07-23 09:03:02 +0200

    update

--- starwars-game/projects/game/src/app/app.component.ts
+++ starwars-game/projects/game/src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { EnemyDTO } from './core/models/enemy.dto';
+import { PersonDTO } from './core/models/person.dto';
+import { miseAJourListEnemiesAction } from './features/enemy/store/actions/enemy.actions';
+import { selectAllEnemies, selectEnemiesALive } from './features/enemy/store/selectors/enemy.selectors';
+import { PeopleService } from './features/people/services/people.service';
+import { ProfileService } from './features/player/services/profile.service';
+import { ApplicationState } from './reducers';
 
 @Component({
   selector: 'game-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'game';
+  people$ = new Observable<PersonDTO[]>();
+  items: PersonDTO[] = [];
+  endOfObservable$ = new Subject<boolean>();
+
+  enemies$ = new Observable<EnemyDTO[]>();
+
+  constructor(private service: ProfileService,
+              private peopleService: PeopleService,
+              private store: Store<ApplicationState>) {
+  }
+
+  ngOnInit() {
+    this.enemies$ = this.store.pipe(
+      select(selectAllEnemies)
+      //select(selectEnemiesALive)
+    );
+
+
+    //this.people$ = this.peopleService.getAll();
+
+    this.peopleService.getAll()
+    .pipe(takeUntil(this.endOfObservable$))
+    .subscribe(items => this.items = items);
+
+    this.people$ = this.peopleService.getAll()
+    .pipe(takeUntil(this.endOfObservable$));
+  }
+
+  ngOnDestroy() {
+    this.endOfObservable$.next(true);
+    this.endOfObservable$.unsubscribe();
+  }
+
+  addNewFakeEnemy() {
+    this.store.dispatch(miseAJourListEnemiesAction());
+  }
 }
--- starwars-game/projects/game/src/app/app.module.ts
+++ starwars-game/projects/game/src/app/app.module.ts
@@ -9,20 +9,33 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
 import { BoolToTextPipe } from './shared/pipes/bool-to-text.pipe';
 import { BoolToColorDirective } from './shared/directives/bool-to-color.directive';
+import { PlayerModule } from './features/player/player.module';
+import { ProfileService } from './features/player/services/profile.service';
+import { Profile2Module } from './features/profile2/profile2.module';
+import { SidePartModule } from './shared/modules/side-part/side-part.module';
+import { ObservablesZoomComponent } from './shared/components/observables-zoom/observables-zoom.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SideBarComponent
+    ObservablesZoomComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    GameModule
+    GameModule,
+    PlayerModule,
+    SidePartModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    })
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent, HeaderComponent, SideBarComponent]
 })
 export class AppModule { }
